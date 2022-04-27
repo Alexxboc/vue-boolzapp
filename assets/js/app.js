89f90c531d1c21e2f155f19785e673adcdb48e61
Alexxboc <boccardi.alessandro@gmail.com>
2022-04-27 17:51:23 +0200

    adjust delete function

--- assets/js/app.js
+++ assets/js/app.js
@@ -233,7 +233,7 @@ const app = new Vue ({
                this.contacts[index].messages.push({click: false, date: '', message: this.newMessage, status: 'sent'}); // "Pusho" nell'Array dei messagi un nuovo oggetto con il messaggio scritto dall'utente
                this.newMessage = '' // Pulisco l'input dopo l'invio del messaggio
                setTimeout(() => {
-                this.contacts[index].messages.push({cliack: false, date: '', message: 'ok', status: 'received'}) // Imposto un timer di un secondo che invii una risposta automatica
+                this.contacts[index].messages.push({click: false, date: '', message: 'ok', status: 'received'}) // Imposto un timer di un secondo che invii una risposta automatica
                }, 1000);
             
            }
@@ -243,7 +243,7 @@ const app = new Vue ({
         //console.log('click');
        const clickValue = this.contacts[this.activeUser].messages[index]
        //console.log(clickValue);
-        clickValue.click !== true ? clickValue.click = true : clickValue.click = false
+        clickValue.click === false ? clickValue.click = true : clickValue.click = false
         // console.log(clickValue.click);
        },
 
